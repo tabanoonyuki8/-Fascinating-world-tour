2d01db9d8006744f2ef7dbdd9a7d979222aa6201
Tabanoon mim <you@example.com>
2023-09-11 22:04:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+  
+
+  return (
+    <>
+<Countries></Countries>
+     
+      <h1>React world tour</h1>
+      
+    </>
+  )
+}
+
+export default App
